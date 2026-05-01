6b606d9f6f75bc176924ff4531e13981991e5f76
AymanFen <aymanfenkouch@gmail.com>
2026-05-01 16:35:22 +0200

    final changes for modelling part

--- src/features/TimeFeatures.py
+++ src/features/TimeFeatures.py
@@ -8,7 +8,7 @@ def ComputeTimeFeatures(df):
     df['AmountZScore'] = (df['LogAmount'] - df['MovingAvg']) / (df['MovingStd'] + 1e-6)
 
     df['TimeDiff']=df.groupby("Account Number")['Time'].diff().dt.total_seconds()/3600
-    df['LogTimeDiff']=np.log(df['TimeDiff'])
+    df['LogTimeDiff'] = np.log(df['TimeDiff'].clip(lower=1e-6))
     
     df['Hoursin'] = np.sin(2 * np.pi * df['Hour'] / 24)
     df['Hourcos'] = np.cos(2 * np.pi * df['Hour'] / 24)
